f84861b28d8fddcd698c24187ca44b904304fb9a
dvishal485 <dvishal485@gmail.com>
2023-09-14 18:09:57 +0530

    view only availiable settings

--- src/App.js
+++ src/App.js
@@ -2,90 +2,98 @@
 import './App.css';
 import './Components/style.scss'
 import Navbar from './Components/Navbar';
-import Footer from './Components/Footer';
+// import Footer from './Components/Footer';
 import Child_int from './Components/Child_int';
 import Child_radio from './Components/Child';
 import settings from './settings.json'; // Import the JSON data
-import { useState } from 'react';
-import Child_toggle from './Components/Child_toggle';
+import { useState, useEffect } from 'react';
+// import Child_toggle from './Components/Child_toggle';
 
 function App() {
 
-  const array=[]
+    const array = []
 
-  const handleClick = (e) => 
-  {
-     console.log("Submitted")
-     console.log(array)
+    const [loading, setLoading] = useState(true);
+    const [mapped_config, setConfigs] = useState({});
 
-     for(let i=0; i<settings.length ;i++)
-     {
-        const id=i+1
-      
+    useEffect(() => {
+        fetch('/get_all_configs').then(res => res.json()).then(data => {
+            let mapped_config = {};
+            data.forEach(element => {
+                mapped_config[element[0]] = element[1];
+            });
 
-     }
-  };
-  
-return (
-    <div className="app">
-      <div className="app-inner">
-        <Navbar />
+            setLoading(false);
+            setConfigs(mapped_config);
+        })
+    }, []);
 
-        <div className="middle">
+    return (
+        <div className="app">
+            <div className="app-inner">
+                <Navbar />
 
-
-          <div className='mid-top'>
-            <h2> Customize Ubuntu Configurations</h2>
-            <h3>You can modify the settings for each type of the following functionalities</h3>
-          </div>
+                <div className="middle">
 
 
-          <div className='mid-mid' style={{ maxHeight: '400px', overflowY: 'auto' }}>
-          {/* //conditionally conder karna hai by using new settings.json */}
-          {settings.map((setting, index) => (
-          setting.value_type === "bool" ? (
-            <Child_radio
-              myArray={array} 
-              id={setting.id}
-              key={index}
-              name={setting.name}
-              type={`Type: ${setting.type}`}
-              description={`${setting.description}`}
-              value_type={`Value Type: ${setting.value_type}`}
-              schema={`Schema: ${setting.schema}`}
-            />
-           ) : (
-            setting.value_type === "int" && (
-              <Child_int
-                myArray={array} 
-                id={setting.id}
-                key={index}
-                name={setting.name}
-                type={`Type: ${setting.type}`}
-                description={`${setting.description}`}
-                value_type={`Value Type: ${setting.value_type}`}
-                schema={`Schema: ${setting.schema}`}
-              />
-            )
-          )
-        ))}
-
-          </div>
+                    <div className='mid-top'>
+                        <h2> Customize Ubuntu Configurations</h2>
+                        <h3>You can modify the settings for each type of the following functionalities</h3>
+                    </div>
 
 
+                    {
+                        loading && ("loading") ||
+                        <div className='mid-mid' style={{ maxHeight: '400px', overflowY: 'auto' }}>
+                            {/* //conditionally conder karna hai by using new settings.json */}
+                            {settings.map((setting, index) => (
+                                mapped_config[setting.id] ? (
+                                    setting.value_type === "bool" ? (
+                                        <Child_radio
+                                            myArray={array}
+                                            id={setting.id}
+                                            key={index}
+                                            name={setting.name}
+                                            type={`Type: ${setting.type}`}
+                                            description={`${setting.description}`}
+                                            value_type={`Value Type: ${setting.value_type}`}
+                                            schema={`Schema: ${setting.schema}`}
+                                        />
+                                    ) : (
+                                        setting.value_type === "int" && (
+                                            <Child_int
+                                                myArray={array}
+                                                id={setting.id}
+                                                key={index}
+                                                name={setting.name}
+                                                type={`Type: ${setting.type}`}
+                                                description={`${setting.description}`}
+                                                value_type={`Value Type: ${setting.value_type}`}
+                                                schema={`Schema: ${setting.schema}`}
+                                            />
+                                        )
+                                    )
+                                ) : null)
+                            )
 
-        </div>
-        {/* <Footer /> */}
+                            }
 
-            {/* <div className='footer'>
+                        </div>}
+
+
+
+                </div>
+                {/* <Footer /> */}
+
+                {/* <div className='footer'>
                 <button onClick={handleClick}>SUBMIT</button>
                 <button>CANCEL</button>
             </div> */}
 
 
-      </div>
-    </div>
-  );
+            </div>
+        </div>
+    );
 }
 
 export default App;
